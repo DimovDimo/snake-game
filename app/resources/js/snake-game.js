f2121a924f237dadaa9a49cd0952d3dbfdf69dd7
DimovDimo <d.dimov1996@gmail.com>
2021-03-08 12:11:41 +0200

    added functions stretchSnake & snakePiece

--- app/resources/js/snake-game.js
+++ app/resources/js/snake-game.js
@@ -2,3 +2,19 @@ const space = document.getElementById("snake-game");
 const snakeSpace = space.getContext("2d");
 
 let snake = [{x: 300, y: 300}];
+
+////////////////////
+stretchSnake();
+////////////////////
+
+function stretchSnake() {
+    snake.forEach(snakePiece);
+}
+
+function snakePiece(piece) {  
+    snakeSpace.fillStyle = "#66FF00";    
+    snakeSpace.fillRect(piece.x, piece.y, 20, 20);
+    
+    snakeSpace.strokestyle = "#568203";
+    snakeSpace.strokeRect(piece.x, piece.y, 20, 20);
+}
