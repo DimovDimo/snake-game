d839060981b335b1f402befa08a2c268bbd1c725
DimovDimo <d.dimov1996@gmail.com>
2021-03-09 12:06:41 +0200

    added function drawSpace

--- app/resources/js/snake-game.js
+++ app/resources/js/snake-game.js
@@ -4,6 +4,7 @@ const snakeSpace = space.getContext("2d");
 let snake = [{x: 300, y: 300}];
 
 ////////////////////
+drawSpace();
 stretchSnake();
 ////////////////////
 
@@ -12,9 +13,17 @@ function stretchSnake() {
 }
 
 function snakePiece(piece) {  
-    snakeSpace.fillStyle = "#66FF00";    
+    snakeSpace.fillStyle = "#66ff00";    
     snakeSpace.fillRect(piece.x, piece.y, 20, 20);
     
     snakeSpace.strokestyle = "#568203";
     snakeSpace.strokeRect(piece.x, piece.y, 20, 20);
+}
+
+function drawSpace() {
+    snakeSpace.fillStyle = "#f2dc7d";    
+    snakeSpace.fillRect(0, 0, space.width, space.height);
+
+    snakeSpace.strokestyle = "#a08d39";
+    snakeSpace.strokeRect(0, 0, space.width, space.height);
 }
