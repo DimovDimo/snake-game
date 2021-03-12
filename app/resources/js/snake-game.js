1a0758f6da2baa847540262afb03a054bac873db
DimovDimo <d.dimov1996@gmail.com>
2021-03-12 12:00:52 +0200

    added function control & key consts

--- app/resources/js/snake-game.js
+++ app/resources/js/snake-game.js
@@ -1,7 +1,17 @@
 const space = document.getElementById("snake-game");
 const snakeSpace = space.getContext("2d");
 
-let snake = [{horizontal: 300, vertical: 300}];
+const UP = 38;
+const DOWN = 40;
+const LEFT = 37;
+const RIGHT = 39;
+
+let snake = [{ horizontal: 300, vertical: 300 }];
+
+let horizontal = 20;
+let vertical = 0;
+
+document.addEventListener("keydown", control);
 
 engine();
 
@@ -16,20 +26,25 @@ function update() {
     engine();
 }
 
+function control(keydown) {
+    let key = keydown.keyCode;
+    //TODO    
+}
+
 function stretchSnake() {
     snake.forEach(snakePiece);
 }
 
-function snakePiece(piece) {  
-    snakeSpace.fillStyle = "#66ff00";    
-    snakeSpace.fillRect(piece.horizontal, piece.vertical, 20, 20);
-    
+function snakePiece(piece) {
+    snakeSpace.fillStyle = "#66ff00";
+    snakeSpace.fillRect(piece.horizontal, piece.vertical, horizontal, horizontal);
+
     snakeSpace.strokestyle = "#568203";
-    snakeSpace.strokeRect(piece.horizontal, piece.vertical, 20, 20);
+    snakeSpace.strokeRect(piece.horizontal, piece.vertical, horizontal, horizontal);
 }
 
 function drawSpace() {
-    snakeSpace.fillStyle = "#f2dc7d";    
+    snakeSpace.fillStyle = "#f2dc7d";
     snakeSpace.fillRect(0, 0, space.width, space.height);
 
     snakeSpace.strokestyle = "#a08d39";
@@ -37,11 +52,11 @@ function drawSpace() {
 }
 
 function motion() {
-    const head = {
-        horizontal: snake[0].horizontal + 20,
-        vertical: snake[0].vertical + 0
+    const piece = {
+        horizontal: snake[0].horizontal + horizontal,
+        vertical: snake[0].vertical + vertical
     };
-    
-    snake.unshift(head);
+
+    snake.unshift(piece);
     snake.pop();
 }
